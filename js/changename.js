b726418e72442ef13519cd145369358bc6049061
mathew36c <mathewogaralde@gmail.com>
2016-08-10 11:13:25 +0800

    update

---
+++ js/changename.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+    $("#img1").hover(function(){
+        $("#cname").text("Jane Hunt");
+        $(".comment").text('"Matt`s analysis was very insightful, detailed, and constructive.  He really understood my script and what I was trying to do with it and brought his practical experience as well as wide knowledge of screenwriting tools to our discussion.  His approach is very hands-on and I appreciated that we met in person and I was able to enjoy the benefits of a creative dialogue with him.  I highly recommend Matt`s services."');
+    });
+    $("#img2").hover(function(){
+        $("#cname").text("Jose Smith");
+        $(".comment").text('"Matt has a sharp and disciplined approach to story structure but also a keen sense of character, nuance, and intention, which is a rare combination.  I felt that he really got what I was going for and provided concrete suggestions as to how to get it across more effectively to my intended audience."');
+    });
+    $("#img3").hover(function(){
+        $("#cname").text("Josh Mccov");
+        $(".comment").text('"Matt provided invaluable feedback to my writing process. His keen understanding of plot, character and theme, and ability to relate to these elements to my story, played a key role in my next rewrite and subsequent success with my script. Matt has a genuine desire to help writers tell great stories. I will definitely reach out to Matt on my next script!"');
+    }); 
+    $("#img4").hover(function(){
+        $("#cname").text("Lu Waldrip");
+        $(".comment").text('"Matt was a key part of my scripts success. He has a keen eye for narrative and character, offering new ideas and helping to make your project the best it could be. Matt is extremely passionate about what he does and is a pleasure to work with!"');    
+    });
+    $("#img5").hover(function(){
+        $("#cname").text("Hanna Conda");       
+        $(".comment").text('"Matt provided invaluable feedback to my writing process. His keen understanding of plot, character and theme, and ability to relate to these elements to my story, played a key role in my next rewrite and subsequent success with my script. Matt has a genuine desire to help writers tell great stories. I will definitely reach out to Matt on my next script!"');     
+    });
+    $("#img6").hover(function(){
+        $("#cname").text("Lorna Gibb");       
+        $(".comment").text('"Matt has a sharp and disciplined approach to story structure but also a keen sense of character, nuance, and intention, which is a rare combination.  I felt that he really got what I was going for and provided concrete suggestions as to how to get it across more effectively to my intended audience."');      
+    });
+    $("#img7").hover(function(){
+        $("#cname").text("Williams Tygart");       
+        $(".comment").text('"Matt was a key part of my scripts success. He has a keen eye for narrative and character, offering new ideas and helping to make your project the best it could be. Matt is extremely passionate about what he does and is a pleasure to work with!"');     
+    });
+    $("#img8").hover(function(){
+        $("#cname").text("George Waldrip");   
+        $(".comment").text('"Matt is analysis was very insightful, detailed, and constructive.  He really understood my script and what I was trying to do with it and brought his practical experience as well as wide knowledge of screenwriting tools to our discussion.  His approach is very hands-on and I appreciated that we met in person and I was able to enjoy the benefits of a creative dialogue with him.  I highly recommend Matts services."');  
+    });  
+    $("#img9").hover(function(){
+        $("#cname").text("Cassandra Smith");   
+        $(".comment").text('"Matt provided invaluable feedback to my writing process. His keen understanding of plot, character and theme, and ability to relate to these elements to my story, played a key role in my next rewrite and subsequent success with my script. Matt has a genuine desire to help writers tell great stories. I will definitely reach out to Matt on my next script!"');    
+    });
+});
